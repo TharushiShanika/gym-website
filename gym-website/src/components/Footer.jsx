ca332d2e5989b5c1ae69c4d16837043985e07e3b
TharushiShanika <shanika2322@gmail.com>
2026-03-19 11:43:32 +0530

    Create Footer.jsx
    Footer

---
+++ gym-website/src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="bg-[#0F172A] text-white py-6 text-center">
+      <p>© 2026 Gym Website. All rights reserved.</p>
+    </footer>
+  );
+}
