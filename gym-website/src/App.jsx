a0a0722380f8fc57a88d8f17d3c335fcad0c77ea
TharushiShanika <shanika2322@gmail.com>
2026-03-19 11:44:35 +0530

    Update App.jsx

--- gym-website/src/App.jsx
+++ gym-website/src/App.jsx
@@ -4,3 +4,18 @@ import About from "./components/About";
 import Services from "./components/Services";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <About />
+      <Services />
+      <Contact />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
