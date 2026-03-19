aaa34403301aa5380edf1b00adcd0ddb98a882bf
TharushiShanika <shanika2322@gmail.com>
2026-03-19 13:29:03 +0530

    Update App.jsx

--- gym-website/src/App.jsx
+++ gym-website/src/App.jsx
@@ -1,14 +1,25 @@
-function App() {
+import React from 'react';
+import { Navbar } from './components/Navbar';
+import { HeroSection } from './components/HeroSection';
+import { AboutSection } from './components/AboutSection';
+import { ServicesSection } from './components/ServicesSection';
+import { MembershipSection } from './components/MembershipSection';
+import { TrainersSection } from './components/TrainersSection';
+import { ContactSection } from './components/ContactSection';
+import { Footer } from './components/Footer';
+export function App() {
   return (
-    <main className="bg-brand-dark font-sans text-white">
+    <div className="min-h-screen bg-neutral-950 text-white font-sans selection:bg-orange-500 selection:text-white">
       <Navbar />
-      <Hero />
-      <div className="bg-brand-light text-brand-dark">
-        <About />
-      </div>
-      <Services />
-      <Contact />
+      <main>
+        <HeroSection />
+        <AboutSection />
+        <ServicesSection />
+        <MembershipSection />
+        <TrainersSection />
+        <ContactSection />
+      </main>
       <Footer />
-    </main>
-  );
+    </div>);
+
 }
