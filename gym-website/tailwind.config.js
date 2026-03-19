fe025b1bc88712bd0bd715229b5ab41188616c8f
TharushiShanika <shanika2322@gmail.com>
2026-03-19 13:34:52 +0530

    Update tailwind.config.js

--- gym-website/tailwind.config.js
+++ gym-website/tailwind.config.js
@@ -1,14 +1,4 @@
-export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {
-      colors: {
-        gold: "#D9A619",
-        bronze: "#261E11",
-        charcoal: "#2D2E2E",
-        offwhite: "#F2F1ED",
-      },
-    },
-  },
-  plugins: [],
-};
+export default {content: [
+  './index.html',
+  './src/**/*.{js,ts,jsx,tsx}'
+],}
