f85fbc64bed6ea1bad13246065c3b06fca27c5d9
TharushiShanika <shanika2322@gmail.com>
2026-03-19 11:44:15 +0530

    Update App.jsx

---
+++ gym-website/src/App.jsx
@@ -0,0 +1,6 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Services from "./components/Services";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
